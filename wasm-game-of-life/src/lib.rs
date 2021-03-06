90e64ad6d1e0f5391b383f39054007333c301c67
Taisuke Miyazaki <imishinist@gmail.com>
2021-03-06 23:13:28 +0900

    render to canvas

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -101,6 +101,18 @@ impl Universe {
         }
         count
     }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
 
 impl fmt::Display for Universe {
